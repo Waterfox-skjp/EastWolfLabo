116798d37d1adb5c2aea7c5196bda9cdca40a68a
水狐 <yuki.skline@gmail.com>
2019-04-03 00:05:16 +0900

    基礎ファイル追加

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<?php
+  $title = '';
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/include/head.php');
+?>
+</head>
+<body>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/include/script.php'); ?>
+</body>
+</html>
