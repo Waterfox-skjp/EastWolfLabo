3b262106aabe2d7da997c940b237ae0f0fa43c2e
水狐 <yuki.skline@gmail.com>
2019-04-21 11:41:45 +0900

    ABOUT(PC) 完了

--- include/header.php
+++ include/header.php
@@ -2,7 +2,7 @@
   function nav_current($path) {
     $now_uri = $_SERVER['REQUEST_URI'];
     if ($path == $now_uri) {
-      echo 'current';
+      echo ' class="current"';
     } else {
       echo '';
     }
@@ -12,9 +12,9 @@
   <div class="inner-wrap">
     <?php
       if ( $_SERVER['REQUEST_URI'] == '/' ) {
-        echo '<h1 class="logo"><a href="/"><img src="/images/common/logo.png" alt="東狼研究所"></a></h1>';
+        echo '<h1 class="logo"><a href="/"><img src="/images/common/logo.png" alt="東狼研究所 EastWolfLaboratory"></a></h1>';
       } else {
-        echo '<div class="logo"><a href="/"><img src="/images/common/logo.png" alt="東狼研究所"></a></div>';
+        echo '<div class="logo"><a href="/"><img src="/images/common/logo.png" alt="東狼研究所 EastWolfLaboratory"></a></div>';
       }
     ?>
     <div id="js-nav-open" class="btn-nav"><img src="/images/common/btn-nav.svg" alt=""></div>
@@ -24,19 +24,19 @@
         <div id="js-nav-close" class="btn-close"><img src="/images/common/btn-close.svg" alt=""></div>
       </div>
       <ul>
-        <li class="<?php nav_current('/');?>">
+        <li <?php nav_current('/');?>>
           <a href="/">HOME</a>
         </li>
-        <li class="<?php nav_current('/projects/');?>">
+        <li <?php nav_current('/projects/');?>>
           <a href="/projects/">PROJECTS</a>
         </li>
-        <li class="<?php nav_current('/about/');?>">
+        <li <?php nav_current('/about/');?>>
           <a href="/about/">ABOUT</a>
         </li>
-        <li class="<?php nav_current('/member/');?>">
+        <li <?php nav_current('/member/');?>>
           <a href="/member/">MEMBER</a>
         </li>
-        <li class="<?php nav_current('/contact/');?>">
+        <li <?php nav_current('/contact/');?>>
           <a href="/contact/">CONTACT</a>
         </li>
       </ul>
